bdc20710269b9fafd450817831d04744404cdf82
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-05 22:55:17 +0200

    setMenuOpen after any click on menu component

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 import "./menu.scss";
 
-export default function Menu() {
+export default function Menu({ menuOpen, setMenuOpen }) {
     return (
-        <div className="menu">
+        <div className={"menu " + (menuOpen && "active")}>
             <ul>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#intro">Home</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#portofolio">Portofolio</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#works">Works</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#testimonials">Testimonials</a>
                 </li>
-                <li>
+                <li onClick={() => setMenuOpen(false)}>
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
