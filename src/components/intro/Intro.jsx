8790385c04663fefa6d0634b747320b983fcb596
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-06 20:21:08 +0200

    added some content to intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,21 @@ import "./intro.scss";
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="Bill" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Dushime Bill Benon</h1>
+                    <h3>Open Sourcerer<span></span></h3>
+                </div>
+                <a href="#portofolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
