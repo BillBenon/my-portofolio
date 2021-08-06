dc71a105d26190a5cdf2ad7be24897512c5437a2
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-06 20:53:09 +0200

    initialized the jsx for portofolio component

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -5,7 +5,20 @@ import "./portofolio.scss";
 export default function Portofolio() {
     return (
         <div className="portofolio" id="portofolio">
-            
+            <h1>Portofolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Desktop App</li>
+                <li>Design</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://th.bing.com/th/id/OIP.C-z28UV0gqt55sAudn4yEAHaEG?pid=ImgDet&rs=1" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
