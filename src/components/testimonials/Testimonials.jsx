99a68c4cf667fafd3e1d853419692a60fe6e5607
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-09 14:17:05 +0200

    styled the first testimonial card container well

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -11,10 +11,11 @@ export default function Testimonials() {
                     <div className="top">
                         <img src="assets/right-arrow.png" className="left" alt="" />
                         <img 
+                            className="user" 
                             src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
                             alt="" 
                         />
-                        <img src="assets/youtube.png" alt="" />
+                        <img className="right" src="assets/youtube.png" alt="" />
                     </div>
                     <div className="center">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
