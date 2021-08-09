7e17867ea88dc6063df6369c98dc4195e58ee34f
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-09 13:55:24 +0200

    initialization of first card in container div in testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -9,10 +9,21 @@ export default function Testimonials() {
             <div className="container">
                 <div className="card">
                     <div className="top">
-                        
+                        <img src="assets/right-arrow.png" className="left" alt="" />
+                        <img 
+                            src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+                            alt="" 
+                        />
+                        <img src="assets/youtube.png" alt="" />
                     </div>
-                    <div className="center"></div>
-                    <div className="bottom"></div>
+                    <div className="center">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Doloribus iure quae ullam veritatis repellat dolores, nobis labore.
+                    </div>
+                    <div className="bottom">
+                        <h3>Stanley</h3>
+                        <h4>Instructor at RCA</h4>
+                    </div>
                 </div>
             </div>
         </div>
